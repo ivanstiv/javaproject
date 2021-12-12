27b985b734c67a26385b623f237988f8e37aa8fd
Ivan Titov <albertduf@mail.ru>
2021-12-12 18:26:26 +0300

    fix

--- HomeWorkApp.java
+++ HomeWorkApp.java
@@ -1,29 +1,25 @@
 public class HomeWorkApp {
-    public static void main(String[] args) {
+    static void main(String[] args) {
         printThreeWords();
         checkSumSign();
         printColor();
         compareNumbers();
     }
 
-    public static void printThreeWords() {
+    static void printThreeWords() {
         System.out.println("Orange");
         System.out.println("Banana");
         System.out.println("Apple");
     }
 
-    public static void checkSumSign() {
+    static void checkSumSign() {
         int a = 3;
         int b = 4;
 
-        if (a+b >= 0) {
-            System.out.println("Сумма положительная");
-        } else {
-            System.out.println("Сумма отрицательная");
-        }
+        (a+b >= 0) ? System.out.println("Сумма положительная") : System.out.println("Сумма отрицательная");
     }
 
-    public static void printColor() {
+    static void printColor() {
         int value = 1;
 
         if (value <= 0) {
@@ -35,14 +31,10 @@ public class HomeWorkApp {
         }
     }
 
-    public static void compareNumbers() {
+    static void compareNumbers() {
         int a = 3;
         int b = 4;
 
-        if (a >= b) {
-            System.out.println("a >= b");
-        } else {
-            System.out.println("a < b");
-        }
+        (a >= b) ? System.out.println("a >= b") : System.out.println("a < b");
     }
 }
